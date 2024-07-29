f5820cf00379e7dee03de04e62267947782e4517
Phong Tran <phongttran01@gmail.com>
2024-07-28 22:29:26 -0500

    Add Entry dataclass

--- generate.py
+++ generate.py
@@ -1,8 +1,53 @@
 import argparse
+from dataclasses import dataclass
 import sys
 import logging
+
+
+
+@dataclass 
+class Entry:
+    source: str
+    targets: list[str]
+    profileType: str
     
-def parse_input_file(filename):
+    
+
+# def identify_special_addresses():
+#   """
+#   Identify the special domains from source and target email addresses 
+#   """
+#   pass
+
+
+# def resolve_symlinks():
+#   """
+#   Eliminate symlinks by following targets
+#   """
+#   pass
+
+# def email_validator():
+#   pass
+
+def assign_new_address(address, address_mapping):
+    address_components = address.split('@')
+    domain = address_components[1]
+    if domain in address_mapping:
+        return f"{address_components[0]}@{address_mapping[domain]}"
+    return address
+
+def reassign_addresses(entries, address_mapping):
+    tmp_entries = dict(entries)
+    for source in tmp_entries:
+        source_address = assign_new_address(source, address_mapping)
+        target_addresses = []
+        for target in tmp_entries[source]:
+            target_addresses.append(assign_new_address(target, address_mapping))
+        tmp_entries[source_address] = target_addresses
+    return tmp_entries
+        
+
+def parse_input_file(filename, entries):
     """
     Parses the input file of key value pairs
     
@@ -12,7 +57,7 @@ def parse_input_file(filename):
     Returns:
         dict: contains the source as the key and list of targets as value
     """
-    
+    res_entries = dict(entries)
     with open(filename, 'r') as f:
         for line_number, line in enumerate(f, start=1):
             newLine = line.strip()
@@ -29,20 +74,21 @@ def parse_input_file(filename):
                 # colon_index >= space_index:
                 entry = newLine.split(' ')
 
-            username = entry[0].strip()
+            source = entry[0].strip()
             
-            if username in entry_to_filename:
-                entry_to_filename[username].append(f"{filename}-{line_number}")
-                logger.warning(f"Entry {username} was found in {entry_to_filename[username]}")
-            else:
-                entry_to_filename[username] = [(f"{filename}-{line_number}")]
+            # if username in entry_to_filename:
+            #     entry_to_filename[username].append(f"{filename}-{line_number}")
+            #     logger.warning(f"Entry {username} was found in {entry_to_filename[username]}")
+            # else:
+            #     entry_to_filename[username] = [(f"{filename}-{line_number}")]
                 
-            entries[username] = []
-            for email in entry[len(entry) - 1].split(','):
-                entries[username].append(email.strip())
+            targets = entry[len(entry) - 1].split(',')
+            entry = Entry(source, targets)
+            res_entries[source] = entry    
+    return res_entries
     
 
-def write_to_file(filename):
+def write_to_file(filename, entries):
     """
     Writes the entries into an LDIF file
     
@@ -53,32 +99,22 @@ def write_to_file(filename):
         None
     """
     with open(filename, 'w') as f:
-        for username in entries:
-            f.write(f"dn: {username}\n")
-            f.write(f"uid: {username}\n")
-
-            if '@' not in username: 
-                f.write(f"mail: {username}@ncsa.illinois.edu\n")    
-            else:
-                f.write(f"mail: {username}\n")
+        for entry in entries:
+            f.write(f"dn: {entry.source}\n")
+            f.write(f"uid: {entry.source}\n")
+            f.write(f"mail: {entry.source}\n")
             
-            if len(entries[username]) == 1:
+            target_key = 'mailRoutingAddress'
+            if len(entry.targets) == 0:
                 f.write(f"profileType: 0\n")
-                target_key = 'mailRoutingAddress'
             else:
                 f.write(f"profileType: 1\n")
                 target_key = 'listMember'
-                
-            for email in entries[username]:
-                if email == 'devnull' or email == '/dev/null':
-                    f.write(f"{target_key}: no-reply@illinois.edu\n") 
-                elif email == 'postmaster':
-                    f.write(f"{target_key}: postmaster@illinois.edu\n")        
-                elif '@' not in email:
-                    f.write(f"{target_key}: {email}@ncsa.illinois.edu\n")
-                else:
-                    f.write(f"{target_key}: {email}\n")
+                            
+            for target in entry.target:
+                f.write(f"{target_key}: {target}")
             f.write("\n")
+            
 def dump():
     """
     Dumps the entries to the console. One line per entry. 
@@ -135,7 +171,7 @@ def main():
     if args.output == None and args.dump == False:
         logger.error("Provide an output option. Run python3 generate.py -h for help")
         sys.exit(1)
-
+        
     for file in args.input:
         parse_input_file(file)
         
@@ -145,10 +181,25 @@ def main():
     if args.dump:
         dump()
 
+
+def test():
+    address_mapping = {
+        "uiuc.edu": "illinois.edu",
+        "ncsa.edu": "ncsa.illinios.edu"
+    }
+    filename = 'forwards.txt' 
+    parse_input_file(filename)
+    print(entries)
+    print("------------------------------------------------")
+    reassign_addresses(entries, address_mapping)
+    print(entries)
+    
+    
 if __name__ == '__main__':
     # GLOBAL VARIABLES
-    entries = {}
+    # entries = {}
     entry_to_filename = {}
+    discard_entries = []
     # LOGGING 
     
     logging.basicConfig(level=logging.WARNING,
@@ -157,4 +208,5 @@ if __name__ == '__main__':
                             logging.StreamHandler()
                         ])
     logger = logging.getLogger(__name__)    
-    main()
+    # main()
+    test()
